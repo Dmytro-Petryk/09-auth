8823e270a5f3ecd7ffd7fa4d0b57ad24dd3f3cac
Dima <dimapetrik416@gmail.com>
2025-06-22 14:22:25 -0500

    1.2

---
+++ components/NoteModal/NoteModal.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import css from './NoteModal.module.css';
+import NoteForm from '../NoteForm/NoteForm';
+
+interface NoteModalProps {
+  onClose: () => void;
+}
+
+const NoteModal = ({ onClose }: NoteModalProps) => {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.code === 'Escape') onClose();
+    };
+    document.addEventListener('keydown', handleKeyDown);
+
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.body.style.overflow = '';
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) onClose();
+  };
+
+  return createPortal(
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClick}
+    >
+      <div className={css.modal}>
+        <NoteForm onClose={onClose} />
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+export default NoteModal;
