2693137ec5c6225bd3ce43b0daa4cfbe28603b26
Dima <dimapetrik416@gmail.com>
2025-07-01 12:52:20 -0500

    1.11

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Note } from '@/types/note';
 
-const BASE_URL = 'https://notehub-public.goit.study';
+const BASE_URL = 'https://notehub-public.goit.study/api';
 const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
@@ -9,7 +9,11 @@ const axiosInstance = axios.create({
     Authorization: `Bearer ${TOKEN}`,
   },
 });
-
+interface FetchParams {
+  search?: string;
+  page?: number;
+  perPage?: number;
+}
 interface NotesResponse {
   data: Note[];
   totalPages: number;
@@ -20,14 +24,22 @@ export const fetchNotes = async (
   perPage = 10,
   search = ''
 ): Promise<NotesResponse> => {
-  const { data } = await axiosInstance.get<NotesResponse>(
-    `/api/notes?page=${page}&perPage=${perPage}&search=${encodeURIComponent(search)}`
-  );
-  return data;
+  try {
+    const params: FetchParams = {
+      ...(search.trim() !== '' && { search: search.trim() }),
+      page,
+      perPage,
+    };
+    const res = await axiosInstance.get<NotesResponse>('/notes', { params });
+    return res.data;
+  } catch (error) {
+    console.error('Failed to fetch notes:', error);
+    throw new Error('Could not fetch notes. Please try again later.');
+  }
 };
 
 export const fetchNoteById = async (id: number): Promise<Note> => {
-  const { data } = await axiosInstance.get<Note>(`/api/notes/${id}`);
+  const { data } = await axiosInstance.get<Note>(`/notes/${id}`);
   return data;
 };
 
@@ -38,7 +50,7 @@ interface CreateNotePayload {
 }
 
 export const createNote = async (note: CreateNotePayload): Promise<Note> => {
-  const { data } = await axiosInstance.post<Note>('/api/notes', note);
+  const { data } = await axiosInstance.post<Note>('/notes', note);
   return data;
 };
 
