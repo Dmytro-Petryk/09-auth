86f96de9cb547eb3106abca59cdfa94bd6848a39
Dima <dimapetrik416@gmail.com>
2025-07-09 10:45:21 -0500

    1.6

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,7 +1,7 @@
 import { fetchNotes } from '@/lib/api';
 import NotesClient from './Notes.client';
 type Props = {
-  params: { slug: string[] };
+  params: Promise<{ slug: string[] }>;
 };
 
 export default async function NotesPage({ params }: Props) {
