f479ce1c1c79b5a7f1d24dbfbe0168585cc5f8cd
Dima <dimapetrik416@gmail.com>
2025-07-09 11:16:44 -0500

    1.7++

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,24 +1,3 @@
-import { Footer } from '@/components/Footer/Footer';
-import { Header } from '@/components/Header/Header';
-import { TanStackProvider } from '@/components/TanStackProvider/TanStackProvider';
-
-export default function RootLayout({
-  children,
-  modal,
-}: {
-  children: React.ReactNode;
-  modal: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>
-        <TanStackProvider>
-          <Header />
-          {children}
-          {modal}
-          <Footer />
-        </TanStackProvider>
-      </body>
-    </html>
-  );
+export default function Layout() {
+  return null;
 }
