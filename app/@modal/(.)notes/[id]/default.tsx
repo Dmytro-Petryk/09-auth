f3435d0796d1b94cc070a484a34b8ed84d033b41
Dima <dimapetrik416@gmail.com>
2025-07-08 20:03:17 -0500

    1.2

---
+++ app/@modal/(.)notes/[id]/default.tsx
@@ -0,0 +1,28 @@
+import { fetchNoteById } from '@/lib/api';
+import {
+  HydrationBoundary,
+  dehydrate,
+  QueryClient,
+} from '@tanstack/react-query';
+import NoteDetailsClient from '../../../notes/[id]/NoteDetails.client';
+interface NoteDetails {
+  params: Promise<{ id: string }>;
+}
+export default async function NoteDetails({ params }: NoteDetails) {
+  const rawId = Array.isArray((await params).id)
+    ? (await params).id[0]
+    : (await params).id;
+  const id = Number(rawId);
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient noteId={id} />
+    </HydrationBoundary>
+  );
+}
