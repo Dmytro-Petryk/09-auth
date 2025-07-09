5e51d1ed534b1c71babf4acffaab0800fafcc2d9
Dima <dimapetrik416@gmail.com>
2025-07-09 11:09:06 -0500

    1.8

---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function PageOn() {
+  return null;
+}
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { fetchNotes } from '@/lib/api';
-import NotesClient from './Notes.client';
+import NotesClient from '../filter/[...slug]/Notes.client';
 type Props = {
   params: Promise<{ slug: string[] }>;
 };
